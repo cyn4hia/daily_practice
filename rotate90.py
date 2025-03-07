b7fa864557337ffe5cca99afcb971c8f47be275d
Cindy Zhou <letcindyknow@gmail.com>
2025-03-07 10:34:45 -0500

    fixed issues

--- rotate90.py
+++ rotate90.py
@@ -1,14 +1,16 @@
 def rotate():
-    list = [[1,2,3], [4,5,6], [7,8,9]]
-
-    col1 = []
-    col2 = []
-    col3 = []
+    list_store = []
+    # for i in range(len(list)):
+    #     list_store.append(list[i])
     
-    for i in range(3):
-        col1.append(list[2-i][0])
-        col2.append(list[2-i][1])
-        col3.append(list[2-i][2])
+    # col1 = []
+    # col2 = []
+    # col3 = []
+
+    # for i in range(3):
+    #     col1.append(list[2-i][0])
+    #     col2.append(list[2-i][1])
+    #     col3.append(list[2-i][2])
 
     # list2 = []
     # list2.append(col1)
