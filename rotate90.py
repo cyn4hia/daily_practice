1eaf97f671de8cc76b8c313d7e6293d1853e9043
Cindy Zhou <letcindyknow@gmail.com>
2025-03-07 10:32:08 -0500

    made m*m

--- rotate90.py
+++ rotate90.py
@@ -1,22 +1,35 @@
-def rotate():
-    list = [[1,2,3], [4,5,6], [7,8,9]]
-
-    col1 = []
-    col2 = []
-    col3 = []
+def rotate(list):
+    list_store = []
+    # for i in range(len(list)):
+    #     list_store.append(list[i])
     
-    for i in range(3):
-        col1.append(list[2-i][0])
-        col2.append(list[2-i][1])
-        col3.append(list[2-i][2])
+    # col1 = []
+    # col2 = []
+    # col3 = []
 
-    list2 = []
-    list2.append(col1)
-    list2.append(col2)
-    list2.append(col3)
-    print(list2)
+    # for i in range(3):
+    #     col1.append(list[2-i][0])
+    #     col2.append(list[2-i][1])
+    #     col3.append(list[2-i][2])
+
+    # list2 = []
+    # list2.append(col1)
+    # list2.append(col2)
+    # list2.append(col3)
+    # print(list2)
+    m = len(list)
     
+    # print(m)
+    for i in range(m):
+        col = []
+        for j in range(m):
+            col.append(list[m-j-1][i])
+        list_store.append(col)
+    print(list_store)
+
+
 
 
 if __name__ == "__main__":
-    rotate()
+    list = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
+    rotate(list)
