2601714682f6bde993d0a8b7f5aebcd8cd2642f0
Cindy Zhou <letcindyknow@gmail.com>
2025-03-07 10:42:32 -0500

    change para names

--- rotate90.py
+++ rotate90.py
@@ -1,4 +1,4 @@
-def rotate(list):
+def rotate(values_list):
     list_store = []
     # for i in range(len(list)):
     #     list_store.append(list[i])
@@ -17,13 +17,14 @@ def rotate(list):
     # list2.append(col2)
     # list2.append(col3)
     # print(list2)
-    m = len(list)
+
+    m = len(values_list)
     
     # print(m)
     for i in range(m):
         col = []
         for j in range(m):
-            col.append(list[m-j-1][i])
+            col.append(values_list[m-j-1][i])
         list_store.append(col)
     print(list_store)
 
@@ -31,5 +32,5 @@ def rotate(list):
 
 
 if __name__ == "__main__":
-    list = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
-    rotate(list)
+    values_list = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
+    rotate(values_list)
