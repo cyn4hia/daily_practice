a754368fa5616b219f4040705cd9690078fd688b
cyn <cyn@mac.lan>
2025-08-22 20:35:24 -0400

    check my new code

--- subtract_product_and_sum.py
+++ subtract_product_and_sum.py
@@ -1,5 +1,15 @@
-def subtract_product_and_sum():
-    print("hello world")
+def subtract_product_and_sum(num):
+    product = 1
+    sum = 0
+    string_num = str(num)
+    
+    for i in range(len(string_num)):
+        sum += int(string_num[i])
+        product *= int(string_num[i])
+    
+    print(product - sum)
+   
 
 if __name__ == "__main__":
-    subtract_product_and_sum()
+    num = 4421
+    subtract_product_and_sum(num)
