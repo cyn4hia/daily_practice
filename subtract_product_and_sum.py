1e200384e5f04588ecefc37b4b68aabe2c8f2421
cyn <cyn@mac.lan>
2025-08-24 13:50:32 -0400

    changes made

--- subtract_product_and_sum.py
+++ subtract_product_and_sum.py
@@ -1,15 +1,15 @@
 def subtract_product_and_sum(num):
+    # Given an integer number n, return the difference between the product of its digits and the sum of its digits.
     product = 1
     sum = 0
     string_num = str(num)
     
-    for i in range(len(string_num)):
-        sum += int(string_num[i])
-        product *= int(string_num[i])
+    for i in string_num:
+        sum += int(i)
+        product *= int(i)
     
-    print(product - sum)
-   
+    return product - sum
 
 if __name__ == "__main__":
     num = 4421
-    subtract_product_and_sum(num)
+    print(subtract_product_and_sum(num))
