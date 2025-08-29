e52b6bc15b792ca549add5f95e5ce7ace23654ee
cyn <cyn@cyns-MacBook-Pro.local>
2025-08-29 07:41:42 -0400

    is pano

--- is_panogram.py
+++ is_panogram.py
@@ -1,6 +1,22 @@
-def is_panogram():
-    return "no"
+def is_panogram(sentence):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    dict = {}
+    for i in alphabet:
+        dict[i] = False
+
+    print(dict)
+    for j in sentence:
+        if dict[j] == False:
+            dict[j] = True
+    
+    for k in alphabet:
+        if dict[k] == False:
+            return False
+
+    return True
 
 if __name__ == "__main__":
-    print(is_panogram())
-
+    sentence = "leetcode"
+    print(is_panogram(sentence))
+    sentence = "thequickbrownfoxjumpsoverthelazydog"
+    print(is_panogram(sentence))
