89d40bf90d5c7fd169da048d7c219f718f6fa75c
cyn <cyn@cyns-MacBook-Pro.local>
2025-09-06 17:57:42 -0400

    vowel substring

--- len_lastword.py
+++ len_lastword.py
@@ -5,7 +5,20 @@ def len_lastword(s):
     s[len(s)-1]
     print("the last word is " + last + " with a length of " + str(len_last))
     
+def test_exception(val:int): 
+    if val < 5:
+        raise ValueError
+    return val
+
 
 if __name__ == "__main__":
-    string = input("sentence: ")
-    len_lastword(string)
+    # string = input("sentence: ")
+    # len_lastword(string)
+    val : int = 4
+    try:
+        test_exception(val)
+        print ("value %d is good", val)
+    except ValueError:
+        print ("value %d shall not less than 5", val)
+    finally:
+        print("end")
